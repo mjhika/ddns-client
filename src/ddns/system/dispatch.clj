92d0858c12926c942b0cb1e6c059ce97c22b81b8
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-01-07 21:21:21 -0500

    add dispatch
    add dispatch function and default case
    
    the default case will dispatch with cloudflare

---
+++ src/ddns/system/dispatch.clj
@@ -0,0 +1,21 @@
+(ns ddns.system.dispatch
+  (:require
+   [ddns.dns.cloudflare.interface :as cloudflare]))
+
+(defn- schedule [f interval]
+  (future (while true
+            (do (Thread/sleep interval)
+                (f)))))
+
+(defmulti dispatch-system :ddns-provider)
+
+(defmethod dispatch-system :default
+  [conf]
+  (schedule #(when (cloudflare/update-record? conf)
+               (cloudflare/update-record conf))
+            300000))
+
+(comment
+  (def c (dispatch-system @ddns.system/system-config))
+
+  (future-cancel c))
